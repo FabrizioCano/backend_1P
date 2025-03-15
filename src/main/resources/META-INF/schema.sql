4fa292d0b2b1bf5aeeee41194b1f30ef5a7b2508
FabrizioCano <fabriziocano7@gmail.com>
2025-03-15 16:14:50 -0300

    first commit

---
+++ src/main/resources/META-INF/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS categoria (
+    idCategoria SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
---
+++ target/prueba/WEB-INF/classes/META-INF/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS categoria (
+    id_categoria SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
